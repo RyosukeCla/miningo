f12d705844088d1526ed22559266fb46ec837c47
RyosukeCla <ryosukeclarinet@gmail.com>
2018-11-10 14:58:20 +0900

    refactorr Database

--- src/database.ts
+++ src/database.ts
@@ -2,32 +2,22 @@ import Collection from './collection'
 import DatabaseAdapter from './adapter'
 
 export default class Database {
-  private collections: any
   private adapter: DatabaseAdapter
 
   constructor(adapter: DatabaseAdapter) {
-    this.collections = {}
     this.adapter = adapter
   }
 
   collection<D>(name: string, schema?: any): Collection<D> {
-    const collection = this.collections[name]
-    if (collection) return collection
-
     const collectionSchema = schema ? {
       type: 'object',
       properties: schema
     } : undefined
 
-    this.collections[name] = new Collection<D>(name, this.adapter, collectionSchema)
-    return this.collections[name]
+    return new Collection<D>(name, this.adapter, collectionSchema)
   }
 
   async drop(name: string): Promise<void> {
     await this.adapter.dropCollection(name)
-    const collection = this.collections[name]
-    if (collection) {
-      this.collections[name] = undefined
-    }
   }
 }

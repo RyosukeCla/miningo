c8efad65f87691e508d19d221090982ff788eb56
RyosukeCla <ryosukeclarinet@gmail.com>
2018-11-10 16:35:59 +0900

    refactor json storage adapter

--- src/adapters/JsonStorageAdapter.ts
+++ src/adapters/JsonStorageAdapter.ts
@@ -4,15 +4,9 @@ import DatabaseAdapter from '../adapter'
 import { BaseDoc } from '../collection'
 import { deepCopy } from '../utils'
 
-interface JsonAdapterCollection {
-  path: string,
-  isLocalLocking: boolean,
-  queue: (() => Promise<any>)[]
-}
-
 export default class JsonAdapter<D> implements DatabaseAdapter<D> {
    collections: {
-    [name: string]: JsonAdapterCollection
+    [name: string]: string
   }
 
   private dataPath: any
@@ -24,104 +18,54 @@ export default class JsonAdapter<D> implements DatabaseAdapter<D> {
     if (!fs.existsSync(this.dataPath)) {
       fs.mkdirSync(this.dataPath)
     }
-
-    this.startJobLoop()
   }
 
-  private async getOrCreateCollection(name: string) {
+  private async getOrCreateCollectionPath(name: string) {
     if (!this.collections[name]) {
       const colPath = path.resolve(this.dataPath, name)
-      const col: JsonAdapterCollection = {
-        path: colPath,
-        isLocalLocking: false,
-        queue: []
-      }
 
-      this.collections[name] = col
+      this.collections[name] = colPath
 
-      await this.doJob(name, async () => {
-        if (!fs.existsSync(col.path)) fs.writeFileSync(col.path, '{}')
-      })
+      if (!fs.existsSync(colPath)) fs.writeFileSync(colPath, '{}')
     }
 
     return this.collections[name]
   }
 
-  private async doJob(name: string, job: (col: JsonAdapterCollection) => Promise<any>): Promise<any> {
-    const col = await this.getOrCreateCollection(name)
-    if (!col) return
-    return await new Promise((resolve, reject) => {
-      col.queue.push(async () => {
-        try {
-          resolve(await job(col))
-        } catch(e) {
-          reject(e)
-        }
-      })
-    })
-  }
-
   public async getJson(collection: string): Promise<any> {
-    const col = await this.getOrCreateCollection(collection)
-    return await this.doJob(collection, async () => {
-      const file = fs.readFileSync(col.path, 'utf8')
-      return JSON.parse(file)
-    })
+    const colPath = await this.getOrCreateCollectionPath(collection)
+    const file = fs.readFileSync(colPath, 'utf8')
+    return JSON.parse(file)
   }
 
   public async setItems(collection: string, items: (D & BaseDoc)[]) {
-    const col = await this.getOrCreateCollection(collection)
-    return await this.doJob(collection, async () => {
-      const file = fs.readFileSync(col.path, 'utf8')
-      const json = JSON.parse(file)
-      items.forEach((item) => {
-        json[item._id] = item
-      })
-      fs.writeFileSync(col.path, JSON.stringify(json))
-      return items
+    const colPath = await this.getOrCreateCollectionPath(collection)
+    const file = fs.readFileSync(colPath, 'utf8')
+    const json = JSON.parse(file)
+    items.forEach((item) => {
+      json[item._id] = item
     })
+    fs.writeFileSync(colPath, JSON.stringify(json))
+    return items
   }
 
   public async removeItems(collection: string, ids: string[]) {
-    const col = await this.getOrCreateCollection(collection)
-    return await this.doJob(collection, async () => {
-      const file = fs.readFileSync(col.path, 'utf8')
-      const json = JSON.parse(file)
-      const items: (D & BaseDoc)[] = []
-      ids.forEach((id) => {
-        const item = json[id]
-        if (item) items.push(deepCopy(item))
-        delete json[id]
-      })
-      fs.writeFileSync(col.path, JSON.stringify(json))
-      return items
+    const colPath = await this.getOrCreateCollectionPath(collection)
+    const file = fs.readFileSync(colPath, 'utf8')
+    const json = JSON.parse(file)
+    const items: (D & BaseDoc)[] = []
+    ids.forEach((id) => {
+      const item = json[id]
+      if (item) items.push(deepCopy(item))
+      delete json[id]
     })
+    fs.writeFileSync(colPath, JSON.stringify(json))
+    return items
   }
 
-  public async dropCollection(name: string) {
-    await this.doJob(name, async (col) => {
-      fs.unlinkSync(col.path)
-      delete this.collections[name]
-    })
-  }
-
-  private startJobLoop() {
-    setTimeout(() => {
-      for(let key in this.collections) {
-        const col = this.collections[key]
-        if (!col || col.queue.length === 0 || col.isLocalLocking) return
-        // lock
-        col.isLocalLocking = true
-
-        const job = col.queue.shift()
-        job().then(() => {
-
-          // unlock
-          col.isLocalLocking = false
-        })
-      }
-
-      this.startJobLoop()
-    }, 1)
+  public async dropCollection(collection: string) {
+    const colPath = await this.getOrCreateCollectionPath(collection)
+    fs.unlinkSync(colPath)
+    delete this.collections[collection]
   }
 }
